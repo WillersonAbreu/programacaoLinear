32be9f5d615a8ac5389e9294de28bce5d20c37b5
Willerson Abreu <willersonabreu@hotmail.com>
2020-03-06 22:59:13 -0300

    Merge com branch sucessores

--- src/SubidaDeEncosta.js
+++ src/SubidaDeEncosta.js
@@ -8,29 +8,20 @@ class subidaDeEncostaClass {
     let novo = [];
     let vn = 0;
 
-    // console.log('Solução inicial', atual['turmasAlocadas']);
-    // console.log('Valor avalia', valorAvalia);
-
     let flag = true;
     while (flag) {
       novo = sucessorClass.sucessor(atual);
-      // console.log(novo['turmasAlocadas']);
       vn = avaliaClass.avalia(novo['turmasAlocadas']);
 
       if (vn < va) {
         atual = novo;
-        // console.log('Sucessor', atual['turmasAlocadas']);
         va = vn;
-        // console.log('Avalia sucessor', va);
       } else {
-        // console.log('Sucessor else', atual['turmasAlocadas']);
-        // console.log('Avalia else', va);
         flag = false;
       }
     }
 
     atual['avaliaSubidaDeEncosta'] = va;
-    // console.log(atual);
     return atual;
   }
 }
